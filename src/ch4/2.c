fd452f68879a68c82f13c866131323603cf66830
hhiroki <h-hiroki@users.noreply.github.com>
2019-02-02 19:05:21 +0900

    ⛳️ch4

---
+++ src/ch4/2.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("%d\n", 100+200);
+    printf("%f\n", 10.0 * 3.0);
+    return 0;
+}
---
+++ src/ch4/7.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("%f\n", 40.0 / 13.0);
+    printf("%d\n", 40 % 13);
+    return 0;
+}
