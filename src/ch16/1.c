3f88a6f1467ff762e28a7d38713a827aeba5f266
hhiroki <gyouzakuuka@gmail.com>
2020-02-24 20:23:23 +0900

    typedef struct

--- src/ch16/1.c
+++ src/ch16/1.c
@@ -1,20 +1,39 @@
 #include <stdio.h>
 #include <string.h>
 
-struct student {
+typedef struct {
     int year;
     int clas;
     int number;
     char name[64];
     double stature;
     double weight;
-};
+} student;
+
+void student_print(student data);
 
 int main(void)
 {
-    struct student data;
+    student data;
 
+    data.year = 3;
+    data.clas = 4;
+    data.number = 18;
     strcpy(data.name, "MARIO");
-    printf("%s\n", data.name);
+    data.stature = 179.2;
+    data.weight = 65.3;
+
+    student_print(data);
     return 0;
+}
+
+void student_print(student data)
+{
+    printf("[学年]: %d\n", data.year);
+    printf("[クラス]: %d\n", data.clas);
+    printf("[出席番号]: %d\n", data.number);
+    printf("[名前]: %s\n", data.name);
+    printf("[身長]: %f\n", data.stature);
+    printf("[体重]: %f\n", data.weight);
+    return;
 }
