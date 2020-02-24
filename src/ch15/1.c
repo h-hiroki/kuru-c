cb608eda33cbf0352ca306ce7c098ec8d624d067
hhiroki <gyouzakuuka@gmail.com>
2020-02-24 19:55:37 +0900

    ch15

---
+++ src/ch15/1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int i1, i2, i3;
+    printf("i1(%p)\n", &i1);
+    printf("i2(%p)\n", &i2);
+    printf("i3(%p)\n", &i3);
+
+    int array[10];
+    printf("array___(%p)\n", array);
+    printf("array[0](%p)\n", &array[0]);
+    printf("array[1](%p)\n", &array[1]);
+    printf("array[2](%p)\n", &array[2]);
+
+    int *p, i;
+    p = &i;
+    *p = 10;
+    printf("*p = %d\n", *p);
+    printf("i = %d\n", i);
+
+    return 0;
+}
